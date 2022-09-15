792aad2900a05a2eba658d6ebbb6b29fdfa5326b
yc4ny <yhugestar@snu.ac.kr>
2022-09-16 01:41:38 +0900

    Added verts_decorated

--- verts.py
+++ verts.py
@@ -1,26 +1,3 @@
-'''
-Copyright 2015 Matthew Loper, Naureen Mahmood and the Max Planck Gesellschaft.  All rights reserved.
-This software is provided for research purposes only.
-By using this software you agree to the terms of the SMPL Model license here http://smpl.is.tue.mpg.de/license
-
-More information about SMPL is available here http://smpl.is.tue.mpg.
-For comments or questions, please email us at: smpl@tuebingen.mpg.de
-
-
-About this file:
-================
-This file defines the basic skinning modules for the SMPL loader which 
-defines the effect of bones and blendshapes on the vertices of the template mesh.
-
-Modules included:
-- verts_decorated: 
-  creates an instance of the SMPL model which inherits model attributes from another 
-  SMPL model.
-- verts_core: [overloaded function inherited by lbs.verts_core]
-  computes the blending of joint-influences for each vertex based on type of skinning
-
-'''
-
 import chumpy
 import lbs
 from posemapper import posemap
